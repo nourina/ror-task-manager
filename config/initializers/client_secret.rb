127ec88ae57eede5981932819d48b13eac0de742
nourina <alimanourin@gmail.com>
2021-07-05 12:29:37 +0530

    Uploaded config and db folder

---
+++ config/initializers/client_secret.rb
@@ -0,0 +1,3 @@
+GOOGLE_API_KEY = "19075371591-5g9jfup9o8356bu4memrrc26ckafms70.apps.googleusercontent.com"
+GOOGLE_API_SECRET = "97OJjLMBPE-oyaCM-7SK0BHO"
+SERVICE_EMAIL_ID = "nourina-banu@trackmanager-318807.iam.gserviceaccount.com"
